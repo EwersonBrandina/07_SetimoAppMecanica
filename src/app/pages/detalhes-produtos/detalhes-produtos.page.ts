aa1fb62c21a3294756a83503697652d1d3b58de4
EwersonBrandina <ewerson.brandina@hotmail.com.br>
2022-09-13 11:19:08 -0300

    Atualização 13/09/2022 - 11:20

---
+++ src/app/pages/detalhes-produtos/detalhes-produtos.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DadosProdutosService } from 'src/app/services/dados-produtos.service';
+
+@Component({
+  selector: 'app-detalhes-produtos',
+  templateUrl: './detalhes-produtos.page.html',
+  styleUrls: ['./detalhes-produtos.page.scss'],
+})
+export class DetalhesProdutosPage implements OnInit {
+
+  img : any = "assets/mecanica.png"
+  public produtoSelecionado : any
+
+  constructor(
+    private route : ActivatedRoute,
+    private produto : DadosProdutosService
+  ) { }
+
+  ngOnInit() {
+    const id : number = Number(this.route.snapshot.paramMap.get('id'))
+    this.produtoSelecionado = this.produto.enviarDadosId(id)
+  }
+
+}
---
+++ src/app/services/dados-produtos.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DadosProdutosService {
+
+  private produtos = [
+    {id: 1, nome: 'Alinhamento', valor: 49.90, garantia: 6},
+    {id: 2, nome: 'Balanceamento', valor: 39.90, garantia: 5},
+    {id: 3, nome: 'Revisão de Freios', valor: 129.90, garantia: 12},
+    {id: 4, nome: 'Revisão de suspensão', valor: 199.90, garantia: 12}
+  ]
+
+  constructor() { }
+
+  enviarTodosDados(){
+    return this.produtos
+  }
+
+  enviarDadosId(id: number){
+    const produtoSelecionado = this.produtos.filter(produto => produto.id === id)
+    return produtoSelecionado [0]
+  }
+}
